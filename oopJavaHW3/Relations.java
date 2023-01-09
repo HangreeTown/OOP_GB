2f05fb97f912860834d719eafaaac1dee10ccca9
HangreeTown <79018265+HangreeTown@users.noreply.github.com>
2023-01-09 11:02:29 +0700

    HomeWork3full

---
+++ oopJavaHW3/Relations.java
@@ -0,0 +1,28 @@
+public enum Relations {
+    ANKLE,
+    AUNT,
+    BROTHER,
+    COUSIN,
+    DAUGHTER,
+    FATHER,
+    FATHER_IN_LOW,
+    GRANDDAUGHTER,
+    GRANDMA,
+    GRANDPA,
+    GRANDSON,
+    GREAT_GRANDMA,
+    GREAT_GRANDPA,
+    HUSBAND,
+    MATHER_IN_LOW,
+    MOTHER,
+    NEPHEW,
+    NIECE,
+    SISTER,
+    SON,
+    STEPDAUGHTER,
+    STEPFATHER,
+    STEPMOTHER,
+    STEPSON,
+    WIFE
+
+}
